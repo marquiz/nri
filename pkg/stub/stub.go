fd493bab693b9e87b4024596d760fad9ae8f484e
Markus Lehtonen <markus.lehtonen@intel.com>
2025-03-18 12:49:30 +0200

    stub: support UpdateNodeResources rpc

--- pkg/stub/stub.go
+++ pkg/stub/stub.go
@@ -169,6 +169,9 @@ type Stub interface {
 	// UpdateContainer requests unsolicited updates to containers.
 	UpdateContainers([]*api.ContainerUpdate) ([]*api.ContainerUpdate, error)
 
+	// UpdateNodeResources requests update of the node resources.
+	UpdateNodeResources(*api.UpdateNodeResourcesRequest) error
+
 	// RegistrationTimeout returns the registration timeout for the stub.
 	// This is the default timeout if the plugin has not been started or
 	// the timeout received in the Configure request otherwise.
@@ -612,6 +615,17 @@ func (stub *stub) UpdateContainers(update []*api.ContainerUpdate) ([]*api.Contai
 		return rpl.Failed, err
 	}
 	return nil, err
+}
+
+// UpdateNodeResources requests an update to the node resources.
+func (stub *stub) UpdateNodeResources(req *api.UpdateNodeResourcesRequest) error {
+	if stub.runtime == nil {
+		return ErrNoService
+	}
+
+	ctx := context.Background()
+	_, err := stub.runtime.UpdateNodeResources(ctx, req)
+	return err
 }
 
 // Configure the plugin.
